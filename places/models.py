f0eb2d1c6ed6f3ad99c20b0cf0dd9604628abb99
Boltasov <31598564+Boltasov@users.noreply.github.com>
2023-12-03 17:28:34 +0200

    Add Event model

--- places/models.py
+++ places/models.py
@@ -1,4 +1,13 @@
 from django.db import models
 
-# Create your models here.
-print("hello!")
+
+class Event(models.Model):
+    title = models.CharField(max_length=500)
+    description_short = models.TextField()
+    description_long = models.TextField()
+    coordinates_lng = models.DecimalField(max_digits=20, decimal_places=15)
+    coordinates_lat = models.DecimalField(max_digits=20, decimal_places=15)
+
+    def __str__(self):
+        return self.title
+
