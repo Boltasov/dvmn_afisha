dadd2f52d035bdad5b7fe39c52b6783df693aacf
Boltasov <31598564+Boltasov@users.noreply.github.com>
2024-01-14 03:45:54 +0200

    Move data from template to view

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -23,7 +23,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.show_afisha)
+    path('', views.show_afisha),
+    path('places/<int:id>/', views.places)
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
