7cf4ea065cf21f64af95cf694884a9dce529805d
Boltasov <31598564+Boltasov@users.noreply.github.com>
2024-01-16 20:13:44 +0200

    Show image preview on admin page

--- places/admin.py
+++ places/admin.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 from .models import Event, Images
 
 
 class ImagesInline(admin.TabularInline):
     model = Images
+
+    readonly_fields = ["get_preview"]
+
+    def get_preview(self, obj):
+        width = obj.img.width
+        height = obj.img.height
+        if height > 200:
+            width = width*(200/height)
+            height = 200
+        return format_html('<img src="{url}" width="{width}" height={height} />'.format(
+            url=obj.img.url,
+            width=width,
+            height=height,
+            )
+    )
 
 
 @admin.register(Event)
