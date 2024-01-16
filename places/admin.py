df41307088201d655293ff940c75fa8fcae50581
Boltasov <31598564+Boltasov@users.noreply.github.com>
2024-01-16 19:58:39 +0200

    Add images to event admin page

--- places/admin.py
+++ places/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 from .models import Event, Images
 
 
-admin.site.register(Event)
+class ImagesInline(admin.TabularInline):
+    model = Images
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    inlines = [
+        ImagesInline,
+    ]
+
+
 admin.site.register(Images)
