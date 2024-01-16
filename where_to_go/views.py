8d8b7de2ccf454bb8089d31a4d514e4b69d84134
Boltasov <31598564+Boltasov@users.noreply.github.com>
2024-01-16 19:55:47 +0200

    Use data from db in view

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,43 +1,44 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from places.models import Event
+from django.forms.models import model_to_dict
+from django.urls import reverse
 
 
 def show_afisha(request):
+    features = []
+    events = Event.objects.all()
+    for event in events:
+        feature = {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [event.coordinates_lng, event.coordinates_lat]
+            },
+            "properties": {
+              "title": event.title,
+              "placeId": event.pk,
+              "detailsUrl": reverse(places, args=[event.pk])
+            }
+          }
+        features.append(feature)
+
     context = {"places_geojson":
       {
         "type": "FeatureCollection",
-        "features": [
-          {
-            "type": "Feature",
-            "geometry": {
-              "type": "Point",
-              "coordinates": [37.62, 55.793676]
-            },
-            "properties": {
-              "title": "«Легенды Москвы",
-              "placeId": "moscow_legends",
-              "detailsUrl": "{% static 'places/moscow_legends.json' %}"
-            }
-          },
-          {
-            "type": "Feature",
-            "geometry": {
-              "type": "Point",
-              "coordinates": [37.64, 55.753676]
-            },
-            "properties": {
-              "title": "Крыши24.рф",
-              "placeId": "roofs24",
-              "detailsUrl": "{% static 'places/roofs24.json' %}"
-            }
-          }
-        ]
+        "features": features
       }
     }
     return render(request, 'index.html', context)
 
 
-def places(request, id):
+def places1(request, id):
     event = Event.objects.get(pk=id)
     return HttpResponse(f"{event}")
+
+
+def places(request, id):
+    event = Event.objects.get(pk=id)
+    response = model_to_dict(event)
+    response["imgs"] = [image.img.url for image in event.images_set.all()]
+    return JsonResponse(response, safe=False, json_dumps_params={'ensure_ascii': False, 'indent': 4})
