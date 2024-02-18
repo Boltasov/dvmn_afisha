3ece3e2fadc6c8f755765827dd9d57d400340276
Boltasov <31598564+Boltasov@users.noreply.github.com>
2024-02-18 14:26:01 +0200

    Add verbose_names in russian

--- places/models.py
+++ places/models.py
@@ -3,11 +3,26 @@ from tinymce.models import HTMLField
 
 
 class Event(models.Model):
-    title = models.CharField(max_length=500)
-    description_short = models.TextField()
-    description_long = HTMLField()
-    coordinates_lng = models.DecimalField(max_digits=20, decimal_places=15)
-    coordinates_lat = models.DecimalField(max_digits=20, decimal_places=15)
+    title = models.CharField(
+        max_length=500,
+        verbose_name="Заголовок"
+    )
+    description_short = models.TextField(verbose_name="Короткое описание")
+    description_long = HTMLField(verbose_name="Длинное описание")
+    coordinates_lng = models.DecimalField(
+        max_digits=20,
+        decimal_places=15,
+        verbose_name="Долгота"
+    )
+    coordinates_lat = models.DecimalField(
+        max_digits=20,
+        decimal_places=15,
+        verbose_name="Широта"
+    )
+
+    class Meta:
+        verbose_name = "Событие"
+        verbose_name_plural = "События"
 
     def __str__(self):
         return self.title
@@ -17,13 +32,19 @@ class Image(models.Model):
     order = models.IntegerField(
         default=0,
         blank=False,
-        null=False,)
+        null=False,
+        verbose_name="Порядок")
 
-    event = models.ForeignKey(Event, on_delete=models.CASCADE)
-    img = models.ImageField()
+    event = models.ForeignKey(
+        Event,
+        on_delete=models.CASCADE,
+        verbose_name="Событие")
+    img = models.ImageField(verbose_name="Изображение")
 
     class Meta:
         ordering = ['order']
+        verbose_name = "Изображение"
+        verbose_name_plural = "Изображения"
 
     def __str__(self):
         return f'{self.order} {self.event}'
